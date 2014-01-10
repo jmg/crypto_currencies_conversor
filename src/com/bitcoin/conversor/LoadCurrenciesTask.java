9c583c35c953317dbb6551dc21679b1276fb4adf
Juan Manuel García <jmg.utn@gmail.com>
2014-01-10 18:22:37 -0300

    more styles

--- src/com/bitcoin/conversor/LoadCurrenciesTask.java
+++ src/com/bitcoin/conversor/LoadCurrenciesTask.java
@@ -4,8 +4,10 @@ import java.util.ArrayList;
 
 import com.bitcoin.conversor.R;
 
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.view.Gravity;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -26,17 +28,18 @@ public class LoadCurrenciesTask extends AsyncTask<Void, Void, ArrayList<Currency
     	return coinCalculator.getCryptoCurrencies();
     }
     
-    public LinearLayout addCurrencyView(CurrencyPrice currency) {
+    public LinearLayout addCurrencyView(CurrencyPrice currency, int index, int size) {
     	
     	LinearLayout currencyContainer = new LinearLayout(this.mainActivity);
     	currencyContainer.setOrientation(1);
-    	currencyContainer.setPadding(0, 0, 0, 5);
+    	currencyContainer.setPadding(0, 5, 0, 5);
+    	currencyContainer.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
     	    	
     	TextView labelTextView = new TextView(this.mainActivity);
-        labelTextView.setTextSize(16);
+        labelTextView.setTextSize(15);
         labelTextView.setText(currency.getName() + " - USD");
         labelTextView.setGravity(Gravity.CENTER_HORIZONTAL);
-        labelTextView.setPadding(0, 0, 0, 10);
+        labelTextView.setPadding(0, 5, 0, 5);
         
         String value = this.coinCalculator.getFloatPrice(currency.getId()).toString();
 
@@ -44,7 +47,15 @@ public class LoadCurrenciesTask extends AsyncTask<Void, Void, ArrayList<Currency
         priceTextView.setTextSize(20);
     	priceTextView.setText("$" + value);
     	priceTextView.setGravity(Gravity.CENTER_HORIZONTAL);
-    	priceTextView.setPadding(0, 0, 0, 10);
+    	priceTextView.setPadding(0, 5, 0, 5);
+    	
+    	String color;
+    	if (index % 2 == 0) {
+    		color = "#B1C9DF";
+    	} else {
+    		color = "#CDD6DE";
+    	}    	
+    	currencyContainer.setBackgroundColor(Color.parseColor(color));
     	
     	currencyContainer.addView(labelTextView);
     	currencyContainer.addView(priceTextView);
@@ -54,12 +65,15 @@ public class LoadCurrenciesTask extends AsyncTask<Void, Void, ArrayList<Currency
     
     protected void onPostExecute(ArrayList<CurrencyPrice> currencies) {
             	
-    	LinearLayout container = (LinearLayout) this.mainActivity.findViewById(R.id.currencies_conatiner_lyt); 
+    	LinearLayout container = (LinearLayout) this.mainActivity.findViewById(R.id.currencies_conatiner_lyt);
         
+    	int index = 0;
+    	int size = currencies.size();
     	for (CurrencyPrice currency: currencies) {
     		
-    		LinearLayout layout = this.addCurrencyView(currency);
+    		LinearLayout layout = this.addCurrencyView(currency, index,  size);
     		container.addView(layout);
+    		index++;
     	}
     }
 
